c3b47943e1909e58aaa5e7edc286f5963304efc9
s.moradi <sinajabermoradi.working@gmail.com>
2024-10-29 01:06:27 -0700

    Add PieChart

--- ShareOfMarket/ChartHub/PieChartHub.cs
+++ ShareOfMarket/ChartHub/PieChartHub.cs
@@ -1,21 +1,38 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using ShareOfMarket.Model;
+using System.Text.Json;
 
 namespace ShareOfMarket.ChartHub;
 public class PieChartHub : Hub
 {
-    private readonly Random random = new();
+    private static readonly double[] _originalValues = { 40, 25, 19, 10, 6 };
+    private readonly Random _random = new();
+    private static readonly double[] _modifiedValues = new double[_originalValues.Length];
 
 
     public async Task SendRandomDataToPieChart()
     {
-        int number;
-        double bitcoin = 57;
-
         while (true)
         {
-            number = random.Next(0, 500);
-            Console.WriteLine($"Sending random number: {number}");
-            await Clients.All.SendAsync("RandomData", number);
+            for (int i = 0; i < _originalValues.Length; i++)
+            {
+                double change = (_random.NextDouble() * 10) - 5;
+                double newValue = _originalValues[i] + (_originalValues[i] * change / 100);
+                _modifiedValues[i] = Math.Round(newValue, 2);
+            }
+
+            Cryptocurrency cryptocurrency = new()
+            {
+                Bitcoin = _modifiedValues[0],
+                Tether = _modifiedValues[1],
+                Ethereum = _modifiedValues[2],
+                Solana = _modifiedValues[3],
+                Bnb = _modifiedValues[4]
+            };
+
+            string jsonConvert = JsonSerializer.Serialize(cryptocurrency);
+            Console.WriteLine($" Sending random number ");
+            await Clients.All.SendAsync("SendRandomData", jsonConvert);
             await Task.Delay(5000);
         }
     }
---
+++ ShareOfMarket/Model/Cryptocurrency.cs
@@ -0,0 +1,9 @@
+﻿namespace ShareOfMarket.Model;
+public class Cryptocurrency
+{
+    public double Bitcoin { get; set; }
+    public double Tether { get; set; }
+    public double Ethereum { get; set; }
+    public double Solana { get; set; }
+    public double Bnb { get; set; }
+}
