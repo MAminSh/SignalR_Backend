8719349c263aab0b2caa2a1933f0b9e3306464e5
s.moradi <sinajabermoradi.working@gmail.com>
2024-10-29 01:11:54 -0700

    Update Chart Hub

--- GoldAndDollerAverage/ChartHub/AvarageChartHub.cs
+++ GoldAndDollerAverage/ChartHub/AvarageChartHub.cs
@@ -10,16 +10,19 @@ namespace GoldAndDollerAverage.ChartHub
 
         public async Task SendRandomDataToAvarageChart()
         {
-            for (int i = 0; i < _gold.Length; i++)
+            while (true)
             {
-                _doller[i] = random.Next(1, 20);
-                _gold[i] = random.Next(40, 60);
+                for (int i = 0; i < _gold.Length; i++)
+                {
+                    _doller[i] = random.Next(1, 20);
+                    _gold[i] = random.Next(40, 60);
+                }
+                Console.WriteLine("Log From BarChart : ");
+                Console.WriteLine($"Doller: [ {_doller[0]}, {_doller[1]}, {_doller[2]}, {_doller[3]}, {_doller[4]}, {_doller[5]}, {_doller[6]}, {_doller[7]}, {_doller[8]},{_doller[9]} ] ");
+                Console.WriteLine($"Gold: [ {_gold[0]}, {_gold[1]}, {_gold[2]}, {_gold[3]}, {_gold[4]}, {_gold[5]}, {_gold[6]}, {_gold[7]}, {_gold[8]},{_gold[9]} ] ");
+                await Clients.All.SendAsync("SendRandomData", _doller, _gold);
+                await Task.Delay(10000);
             }
-            Console.WriteLine("Log From BarChart : ");
-            Console.WriteLine($"Doller: [ {_doller[0]}, {_doller[1]}, {_doller[2]}, {_doller[3]}, {_doller[4]}, {_doller[5]}, {_doller[6]}, {_doller[7]}, {_doller[8]},{_doller[9]} ] ");
-            Console.WriteLine($"Gold: [ {_gold[0]}, {_gold[1]}, {_gold[2]}, {_gold[3]}, {_gold[4]}, {_gold[5]}, {_gold[6]}, {_gold[7]}, {_gold[8]},{_gold[9]} ] ");
-            await Clients.All.SendAsync("SendRandomData", _doller, _gold);
-            await Task.Delay(10000);
         }
     }
 }
